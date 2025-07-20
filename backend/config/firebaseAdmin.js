3602a0283629df39b197b3acf6b23222c1564472
HarshitAggarwal10 <gauravaggarwal89280@gmail.com>
2025-07-20 22:32:16 +0530

    did changes in backend

--- backend/config/firebaseAdmin.js
+++ backend/config/firebaseAdmin.js
@@ -1,12 +1,6 @@
 const admin = require("firebase-admin");
-
-if (!process.env.FIREBASE_SERVICE_ACCOUNT_BASE64) {
-  throw new Error("FIREBASE_SERVICE_ACCOUNT_BASE64 env variable not set");
-}
-
-const serviceAccount = JSON.parse(
-  Buffer.from(process.env.FIREBASE_SERVICE_ACCOUNT_BASE64, "base64").toString("utf8")
-);
+const path = require("path");
+const serviceAccount = require(path.resolve(__dirname, "./firebaseServiceAccount.json"));
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
