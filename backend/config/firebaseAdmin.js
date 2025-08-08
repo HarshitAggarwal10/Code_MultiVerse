fdb66c0a7da15b8daaaddd0bb2f18f3b4cf3ed66
HarshitAggarwal10 <gauravaggarwal89280@gmail.com>
2025-08-08 21:32:48 +0530

    solved backend folder issue

--- backend/config/firebaseAdmin.js
+++ backend/config/firebaseAdmin.js
@@ -1,10 +1,11 @@
 const admin = require("firebase-admin");
+const path = require("path");
 
-if (!process.env.FIREBASE_SERVICE_ACCOUNT_JSON) {
-  throw new Error("FIREBASE_SERVICE_ACCOUNT_JSON is not set in environment variables");
+if (!process.env.FIREBASE_CREDENTIAL_PATH) {
+  throw new Error("FIREBASE_CREDENTIAL_PATH is not set in environment variables");
 }
 
-const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT_JSON);
+const serviceAccount = require(path.resolve(process.env.FIREBASE_CREDENTIAL_PATH));
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
